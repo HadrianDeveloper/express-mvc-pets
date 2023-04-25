c0e7bb60bbb0375c9024cdd675a5b159ff46b805
HadrianDeveloper <hadrian.developer@outlook.com>
2023-04-25 16:45:06 +0100

    select all owners

--- models/owners-model.js
+++ models/owners-model.js
@@ -5,12 +5,26 @@ exports.selectOwnerById = (id) => {
         .then((owner) => JSON.parse(owner))
 };
 
+
+//solution 1 - from scratch
 exports.selectAllOwners = () => {
     return readdir('./data/owners')
         .then((fileList) => {
             const allOwners = fileList.map((file) => {
                 return readFile(`./data/owners/${file}`, 'utf8')
                     .then((file) => JSON.parse(file))
+            })
+            return Promise.all(allOwners)
+        })
+        .then((allOwners) => allOwners)
+};
+
+//Solution 2 - reusing this.selectOwnerById
+exports.selectAllOwners2 = () => {
+    return readdir('./data/owners')
+        .then((filelist) => {
+            const allOwners = filelist.map((file) => {
+                return this.selectOwnerById(file.slice(0, -5))
             })
             return Promise.all(allOwners)
         })
